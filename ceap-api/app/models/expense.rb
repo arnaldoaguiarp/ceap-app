cf7d5e9dbc9a5101e2067078f63359eb2358a3d1
Arnaldo Aguiar <arnaldoaguiarp@gmail.com>
2025-06-23 22:08:51 -0300

    Adjustment in the validation of the Expense model

--- ceap-api/app/models/expense.rb
+++ ceap-api/app/models/expense.rb
@@ -1,12 +1,12 @@
 class Expense < ApplicationRecord
   belongs_to :deputy
-  
+
   validates :deputy, presence: true
   validates :issue_date, presence: true
   validates :supplier, presence: true
-  validates :net_value, presence: true, numericality: { greater_than: 0 }
-  validates :document_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
-  
+  validates :net_value, presence: true, numericality: true 
+  validates :document_url, format: { with: URI::DEFAULT_PARSER.make_regexp }, allow_blank: true
+
   scope :by_date_range, ->(start_date, end_date) { where(issue_date: start_date..end_date) }
   scope :by_supplier, ->(supplier) { where(supplier: supplier) }
   scope :ordered_by_value, -> { order(net_value: :desc) }
